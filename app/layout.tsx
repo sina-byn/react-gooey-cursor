59bc0db2acc4651b245bf454ae9cb2d13f52b0ac
sina-byn <sina.sina.bayan@protonmail.com>
2026-08-12 13:17:29 +0330

    feat: initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+
+import './globals.css';
+
+import { Header } from '@/components/header';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'React Slot',
+  description: 'Vue-style slot composition for React components, simple and flexible.',
+  verification: {
+    google: '0aVU7hmFQkCiP7NbGmnl6Zelf2_srz7qeoZG927OvN0',
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en' className='dark'>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
