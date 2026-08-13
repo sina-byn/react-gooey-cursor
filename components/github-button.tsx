4a2ddb51f7481e71f4874f46229714acc772092f
sina-byn <sina.sina.bayan@protonmail.com>
2026-08-13 18:39:07 +0330

    chore: github-button aria-label text corrected

--- components/github-button.tsx
+++ components/github-button.tsx
@@ -8,7 +8,7 @@ export function GithubButton() {
       data-size='large'
       data-show-count={false}
       data-icon='octicon-star'
-      aria-label='Star sina-byn/react-slot on GitHub'
+      aria-label='Star sina-byn/react-gooey-cursor on GitHub'
       href='https://github.com/sina-byn/react-gooey-cursor'
       data-color-scheme='no-preference: dark; light: dark; dark: dark;'
     >
